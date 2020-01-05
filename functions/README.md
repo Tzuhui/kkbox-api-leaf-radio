55102ac99acd9f5b08e54409f60cacaaaed9b598
方威 <fangwei@fangweide-MacBook-Pro.local>
2020-01-05 13:49:15 +0800

    add kkOauth 取得 token

---
+++ functions/README.md
@@ -0,0 +1,32 @@
+# 部署方式
+
+## firebase 設置
+定價方案須為 **Blaze** 或 **Flame**
+
+## 設置環境變數
+```
+functions:config:set kkbox.app_id=YOUR_APP_ID
+functions:config:set kkbox.secret=YOUR_APP_SECRET
+```
+
+## 部署
+```
+firebase deploy --only functions
+```
+## 
+### 取得 token 
+
+#### method
+- GET
+#### endpoint
+```
+https://YOUR_REGION-YOUR_PROJECT_ID.cloudfunctions.net/kkOauth
+```
+#### response
+```
+{
+    "access_token": "access token",
+    "token_type": "Bearer",
+    "expires_in": 2592000
+}
+```
--- functions/functions/index.js
+++ functions/functions/index.js
@@ -1,5 +1,8 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const axios = require('axios');
+const qs = require('qs');
+const cors = require('cors')({ origin: true });
 
 admin.initializeApp();
 exports.stats = functions.database.ref('/requestSongs/{pushId}/songInfo')
@@ -22,3 +25,22 @@ exports.stats = functions.database.ref('/requestSongs/{pushId}/songInfo')
     })
     return songRecord;
   })
+
+exports.kkOauth = functions.https.onRequest(async (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Methods', 'GET');
+  res.set('Access-Control-Allow-Headers', 'Content-Type')
+  const getToken = await axios({
+    method: 'post',
+    url: 'https://account.kkbox.com/oauth2/token',
+    data: qs.stringify({
+      grant_type: 'client_credentials',
+      client_id: functions.config().kkbox.app_id,
+      client_secret: functions.config().kkbox.secret,
+    })
+  }).then(async (response) => {
+    return res.send(response.data);
+  }).catch(err => {
+    res.send('error: ' + err);
+  })
+})
